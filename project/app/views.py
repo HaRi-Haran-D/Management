29394a99209023a599c89a47a1e96cf168bc0663
HaRi-Haran-D <hvj58128@gmail.com>
2025-12-22 22:05:27 +0530

    project update

--- project/app/views.py
+++ project/app/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -17,7 +19,7 @@ def home(request):
             messages.error(request, "Invalid username or password.")
             return redirect('home')
     else:
-        return render(request, 'index.html', {})
+        return render(request, 'index.html', {'records': records})
 
 def logout_user(request):
     logout(request)
